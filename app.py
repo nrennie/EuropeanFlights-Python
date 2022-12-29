3c2fa6d049f22465dcbfa9b53a4849a240b54c4b
nrennie <nrennie35@gmail.com>
2022-12-29 01:26:27 +0000

    finish initial app

--- app.py
+++ app.py
@@ -1,53 +1,79 @@
 from shiny import ui, render, App
 import plotnine as gg
 import pandas as pd
+from mizani.breaks import date_breaks
+from mizani.formatters import date_format
 
-app_ui = ui.page_fluid(
-  ui.panel_title("European Flights"),
-  ui.markdown(
-        """
-        The number of flights arriving or leaving from European airports saw a dramatic decrease with the onset of the Covid-19 pandemic in March 2020. Amsterdam - Schipol remains the busiest airport, averaging 1,150 flights per day since January 2016.
-        
-        Data: [Eurocontrol](https://ansperformance.eu/data/)
+# read in data
+flights = pd.read_csv('flights_data.csv')  
 
-        """
-    ),
-  ui.row(
-    # bar chart output
-    ui.column(5), 
-    # area chart output
-    ui.column(5), 
-    # controls
-    ui.column(2, 
+# Function for UI
+def create_ui(data: pd.DataFrame):
+  # create our ui object
+  app_ui = ui.page_fluid(
+    ui.panel_title("European Flights"),
       ui.markdown(
-        """
-        ### **Controls**
-        
-        Use the selectors below to choose a date range and set of countries to explore.
-
-        """
+          """
+          The number of flights arriving or leaving from European airports saw a dramatic decrease with the onset of the Covid-19 pandemic in March 2020. Amsterdam - Schipol remains the busiest airport, averaging 1,150 flights per day since January 2016.
+          
+          Data: [Eurocontrol](https://ansperformance.eu/data/)
+  
+          """
       ),
-      ui.input_date_range(
-        "date", "Date",
-        start = "2016-01-01",
-        end = "2022-05-31",
-        min = "2016-01-01",
-        max = "2022-05-31"
-      ),
-      ui.input_checkbox_group(
-          "country", "Country",
-          choices=["Belgium", "France", "Ireland", "Luxembourg", "Netherlands", "United Kingdom"],
-          selected=["Belgium", "France", "Ireland", "Luxembourg", "Netherlands", "United Kingdom"]
+    ui.row(
+        # bar chart output
+        ui.column(10,
+          ui.output_plot("barplot")
+        ), 
+        # controls
+        ui.column(2, 
+          ui.markdown(
+            """
+            ### **Controls**
+            
+            Use the selectors below to choose a set of countries to explore.
+    
+            """
+          ),
+          ui.input_checkbox_group(
+              "country", "Country",
+              choices=["Belgium", "France", "Ireland", "Luxembourg", "Netherlands", "United Kingdom"],
+              selected=["Belgium", "France", "Ireland", "Luxembourg", "Netherlands", "United Kingdom"]
+          )
         )
     )
   )
-)
+  return app_ui
 
-def server(input, output, session):
-    @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+ui_obj = create_ui(flights)
 
-# This is a shiny.App object. It must be named `app`.
-app = App(app_ui, server)
+# Function to make the plot
+def create_plot(data):
+  plot = (
+    gg.ggplot(data = data, mapping = gg.aes(x = 'Date', y='Total', fill='Country')) +
+      gg.geom_col() +
+      gg.theme_minimal() +
+      gg.labs(x = "", y = "Total number of flights per week", title="Total number of flights per week") +
+      gg.scale_x_datetime(breaks=date_breaks('1 years'), labels=date_format('%Y')) +
+      gg.scale_fill_brewer(type="qual", palette = "Dark2") +
+      gg.theme(panel_grid_major_x = gg.element_blank(),
+               panel_grid_minor_x = gg.element_blank())
+  )
+  return plot.draw()
+
+# Function for the server
+def create_server(data):
+  def f(input, output, session):
+  
+    @output(id = "barplot")
+    @render.plot
+    def plot():
+      country = list(input.country())
+      new_data = data[data['Country'].isin(country)]
+      plot = create_plot(new_data)
+      return plot
+  return f
+
+server = create_server(flights)
+
+app = App(ui_obj, server)
